bd67726488d45b2cef7120f25935d93d0187df60
yerkemakhanbet <erkebylan98@gmail.com>
2024-01-30 13:48:17 +0600

    comment

--- src/BankingApplication.java
+++ src/BankingApplication.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+// need to add deposit calculator
 public class BankingApplication {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
